bfd34754ea0811555ceb72b70663a62809fcad02
10maurycy10 <10maurycy10@gmail.com>
2024-10-30 19:39:51 -0600

    Comment code

--- main.c
+++ main.c
@@ -12,9 +12,16 @@
 #define PORTA_CS (1 << 7)
 
 
-// SD card
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// Low level SD card driver, supports SDSC, SDHC, and SDXC cards that work   //
+// with SD version 2 or higher. Uses a block size of 512 (0x200) bytes       //
+// regardless of the SD card used. The flash inside usually has a block size //
+// of 512 or higher, so this doesn't impact lifespan.                        //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
 
-// SPI trancieve primitiv
+// SPI tranceive function
 uint8_t sd_xfer(uint8_t data) {
 	SPI0.DATA = data;
 	while (~SPI0.INTFLAGS & 1 << 7) ;
@@ -22,6 +29,7 @@ uint8_t sd_xfer(uint8_t data) {
 	return SPI0.DATA;
 }
 
+// Flash LED to indicate problem with the card.
 void sd_timeout() {
 	while (1) {
 		PORTC.OUT ^= 1 << 2;
@@ -29,6 +37,8 @@ void sd_timeout() {
 	}
 }
 
+// Read a R1 format response, the other formats are just R1 with some data 
+// tacked on, which can be read with sd_xfer(). 
 uint8_t sd_get_r1() {
 	uint16_t count = 0;
 	while (1) {
@@ -39,6 +49,9 @@ uint8_t sd_get_r1() {
 	}
 }
 
+// Send a command to the card.
+// The CRC will be ingored once initialized for SPI mode, but a correct
+// checksum is needed during initilization.
 void sd_command(uint8_t cmd, uint32_t arg, uint8_t crc) {
 	sd_xfer(cmd|0x40);
 	sd_xfer((uint8_t)(arg >> 24));
@@ -48,18 +61,23 @@ void sd_command(uint8_t cmd, uint32_t arg, uint8_t crc) {
 	sd_xfer(crc|0x01);
 }
 
-
+// Initialize the card.
+// Use on start up and after sd_power_off() before reading or writing.
 void sd_init() {
 	// Power on the card
+	PORTA.DIRSET = 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7; 
 	PORTC.OUTSET = PORTC_E_CARD;
+	_delay_ms(10);
 	PORTA.OUTSET = PORTA_CS;
-	_delay_ms(1);
+	_delay_ms(10);
 
-	// Send 80 dummy clock cycles with the card deselected
+	// Send 80 clock cycles with the card deselected
+	// This lets the card know we're ready to start sending data.
 	for (int i = 0; i < 10; i++) sd_xfer(0xff);
 	_delay_ms(1);
 
-	// Send the initialization command
+	// CMD0
+	// Reset the card to a known state
 	PORTA.OUTCLR = PORTA_CS;
 	_delay_ms(1);
 	sd_command(0, 0, 0x94);
@@ -67,7 +85,8 @@ void sd_init() {
 	sd_get_r1();
 
 	// CMD8
-	// This is the voltage check, but required on newer cards
+	// This is supposed to be the voltage check command, but newer cards
+	// will not work if this is not run.
 	sd_command(8, 0x1AA, 0x87);
 	if (sd_get_r1() != 0x01) sd_timeout();
 	if (sd_xfer(0xFF) != 0x00) sd_timeout();
@@ -75,23 +94,31 @@ void sd_init() {
 	if (sd_xfer(0xFF) != 0x01) sd_timeout();
 	if (sd_xfer(0xFF) != 0xAA) sd_timeout();
 
-	// Card initialization loop
+	// Repeatedly try to initialize the card until it exits the idle state.
+	// This always takes a few attemps, I don't really know why.
 	int done = 0;
+	int timeout = 10000; // 10 seconds
 	while (!done) {
 		// ACMD 41
 		sd_command(55, 0x0, 0x65);
 		sd_get_r1();
 		sd_command(41, 0x40000000, 0x77);
+		
 		uint8_t status = sd_get_r1();
 		if (status == 0x00) done = 1;
 		else if (status == 0x01) continue;
-		else sd_timeout(); // Broken or very old card.
-		_delay_ms(1); // Wait for it to initialize
+		else sd_timeout(); // Broken or very old card. (before SD ver. 2)
+		
+		// Give up if the card doesn't initialize
+		if (timeout-- == 0) sd_timeout(); 
+
+		_delay_ms(1);
 		
 	}
 
 	// At this point we know that the card supports SD V2 or higher
-	// Read OCR for addressing information
+	// Some cards use byte addressing by default, so we read the OCR
+	// register to check.
 	sd_command(48, 0x0, 0x65);	
 	sd_get_r1(); // First byte is reserved
 	uint8_t OCR[4];
@@ -100,42 +127,80 @@ void sd_init() {
 	OCR[2] = sd_xfer(0xFF);
 	OCR[3] = sd_xfer(0xFF);
 	sd_xfer(0xFF); // Final byte is reseved
-	// Bit 30 of the OCR is set for SDHC and XC cards
+	// Bit 30 of the OCR is set for block addressed cards (HC/XC)
 	uint8_t CCR = (OCR[0] >> 6) & 1;
 
-	// Low capacity cards use byte addressing by default, switch to 512 byte blocks for consistancy.
+	// Switch the byte addressed cards to block addressing, using the
+	// same 512 byte block size as the always block addressed cards
+	// for consistancy
 	if (!CCR) {
 		sd_command(16, 0x200, 0x0);
 		sd_get_r1();
 	}
 }
 
-void card_power_off() {
-	// Specification says to give 8 clock cycles after finishing operations, more shoun't hurt.
+// Disconnect power from the SD card to improve battery life.
+void sd_power_off() {
+	// The specification says to give the card 8 clock cycles after
+	// finishing operations, more shoun't hurt.
 	for (int i = 0; i < 10; i++) sd_xfer(0xFF);
 	_delay_ms(1);
-	// Actually power off card
+	// Actually cut power
 	PORTC.OUTCLR = PORTC_E_CARD;
+	PORTA.DIRCLR = 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7; 
 }
 
-// These functions will be called from the filesystem library
-DSTATUS disk_initialize (BYTE pdrv) {
-	return 0;
-};
-
+// Low level read and write primitives
 void read_block(uint8_t* buff, uint32_t sector) {
+	// Send read command
 	sd_command(17, sector, 0);
-	sd_get_r1(); // Resonse
+	sd_get_r1();
 	
-	if (sd_get_r1() != 0xfe) sd_timeout(); // Read token
-	for (int i = 0; i < 512; i++) { // Read data
+	// Recieve data
+	if (sd_get_r1() != 0xfe) sd_timeout();
+	for (int i = 0; i < 512; i++) {
 		buff[i] = sd_xfer(0xff);
 	}
 	
-	sd_xfer(0xff); sd_xfer(0xff); // Discard CRC
+	// Discard the CRC
+	sd_xfer(0xff); sd_xfer(0xff);
 }
 
-// Read part of a block
+void write_block(uint8_t* buff, uint32_t sector) {
+	// Send write command
+	sd_command(24, sector, 0);
+	sd_get_r1();
+	
+	// Specification says to give 8 cycles before sending data
+	sd_xfer(0xff); 
+	
+	// Send the start token
+	sd_xfer(0b11111110);
+	
+	// Send the data
+	for (int i = 0; i < 0x200; i++) {
+		sd_xfer(buff[i]);
+	}
+	
+	// Send dummy CRC
+	sd_xfer(0xff); sd_xfer(0xff);
+
+	// Wait for completion
+	sd_get_r1();
+	while (sd_xfer(0xff) == 0x00) ; 
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+// Filesystem driver interface, called by the FatFs library in fs/.         //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
+// We'll manualy initialize the card before mounting
+DSTATUS disk_initialize (BYTE pdrv) {return 0;}
+
+// Multiblock read. We don't care about speed, so using a bunch of single
+// block commands is just fine.
 DRESULT disk_read (BYTE drive, BYTE* buff, LBA_t sector, UINT count) {
 	for (int i = 0; i < count; i++) {
 		read_block(&buff[512*i], sector + i);
@@ -143,25 +208,9 @@ DRESULT disk_read (BYTE drive, BYTE* buff, LBA_t sector, UINT count) {
 	return 0;
 };
 
-void write_block(uint8_t* buff, uint32_t sector) {
-	sd_command(24, sector, 0); // Write block command
-	sd_get_r1(); // Read responce
-	sd_xfer(0xff); // Give it a moment
-	sd_xfer(0b11111110); // Start token
-	
-	for (int i = 0; i < 0x200; i++) {
-		sd_xfer(buff[i]);
-	}
-	
-	// Send dummy CRC
-	sd_xfer(0xff); sd_xfer(0xff);
-	// Wait for completion
-	sd_get_r1();
 
-	while (sd_xfer(0xff) == 0x00) ; 
-}
-
-int bytes_written = 0;
+// Multiblock write. We don't care about speed, so using a bunch of single
+// block commands is just fine.
 DRESULT disk_write (BYTE drive, const BYTE* buff, LBA_t sector, UINT count) {
 	for (int i = 0; i < count; i++) {
 		write_block(&buff[0x200*i], sector);
@@ -169,21 +218,30 @@ DRESULT disk_write (BYTE drive, const BYTE* buff, LBA_t sector, UINT count) {
 	return 0;
 };
 
+// This function tells the library the block size for reading (SECTOR_SIZE)
+// and writing (BLOCK_SIZE, in multiples of SECTOR_SIZE)
 DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff) {
 	if (cmd == GET_SECTOR_SIZE) *((WORD*)buff) = 0x200;
 	if (cmd == GET_BLOCK_SIZE) *((DWORD*)buff) = 1;
 	return 0;
 }
 
+// The read/write primitives are blocking, nothing to do here.
 DSTATUS disk_status (BYTE pdrv) {
-	return 0; // Access is blocking, no need to check.
+	return 0; 
 }
 
+// Without an RTC, we have no way of knowing the real time, so 
+// just use an blatantly bogus date (1980)
 DWORD get_fattime (void) {
-	return 0; // Don't have a clock ;(
+	return 0; // don't have a clock :(
 }
 
-// Fluxgate measurement
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+// Magnetic field measurement.                                              //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
 
 void measure() {
 	// Turn on the amplifier
@@ -200,10 +258,27 @@ void measure() {
 	PORTC.OUTCLR = PORTC_DRIVE_COIL | PORTC_E_SENSOR;
 }
 
-// Main
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+// Main loop.                                                               //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
 
 FATFS fs;
 FIL fd;
+
+uint32_t lines_written = 0;
+
+void write_banner() {
+	f_puts("\n,,Fluxgate datalogger: restarted, numbering will be reset.\n", &fd);
+	f_sync(&fd);
+}
+void write_datapoint(int64_t measurement) {
+	f_printf(&fd, "%ld,%lld,\n", lines_written, measurement); 
+	f_sync(&fd);
+	lines_written++;
+}
+
 
 int main(void) {
 	PORTC.DIRSET = 0xFF; // LED
@@ -211,22 +286,18 @@ int main(void) {
 	PORTA.DIRSET = 1 << 4 | 1 << 5 | 1 << 6 | 1 << 7; // Sd card spi pins
 	SPI0.CTRLA = 1 << 5 | 0x3 << 1 | 1; // SPI: Master, max prescaler, enabled
 	
-	sd_init(); // Setup the card
+	// Write the startup banner to the card
+	sd_init();
+	if (f_mount(&fs, "", 1)) sd_timeout();
+	if (f_open(&fd, "/FLUXGATE.CSV", FA_WRITE | FA_OPEN_APPEND)) sd_timeout();
+	write_banner();
 
-	// Write a test file
-	if (f_mount(&fs, "", 1)) while (1);
-	_delay_ms(100);
-	if (f_open(&fd, "/TEXT.TXT", FA_WRITE | FA_CREATE_ALWAYS)) while (1);
-	_delay_ms(100);
-	int written = 0;
-	if (f_write(&fd, "Hello world!", 12, &written)) while (1);
-	_delay_ms(100);
-	if (f_close(&fd)) while (1);
-	_delay_ms(100);
-	
+	// Flash LED	
 	while (1) {
 		PORTC.OUT ^= 1 << 2;
+		
 		_delay_ms(100);
+		
 	}
 	return 0;
 }
